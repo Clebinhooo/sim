03ecb937d2c8dfcbc182fb97fdd7be5e0649c461
Clebinhooo <152666409+Clebinhooo@users.noreply.github.com>
2024-06-26 14:33:47 -0300

    Add files via upload

---
+++ php.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>apenas</title>
+</head>
+<body>
+    <?php
+    $valor = $_GET["valor"];
+    $salario = $_GET["salario"];
+    $anos = $_GET["anos"];
+    $anos = $anos * 12;
+    $prestacao = $valor / $anos;
+    if (($salario * 30 / 100)< $prestacao) {
+        echo "Empréstimo negado";
+    }
+    else {
+        echo "Empréstimo aceito,  irá pagar $prestacao por mês";
+    }
+    ?>
+</body>
+</html>
